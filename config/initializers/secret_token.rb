e8982522032a61f4152d9a90ef6f0914b7ba8ba2
Stephen Wright <wright.net@gmail.com>
2011-03-26 23:03:53 -0500

    create initial application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseIssueWithRspec::Application.config.secret_token = 'c0c9eb1706c7aa17b61a63b2367f1870821fab5b6e8df6a5638c8dcd34574df7b19bc662352fa6918bde6401951efae76a2bf1f73ef2c4dcb62f53935dc1b35b'
